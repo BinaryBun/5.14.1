2319fbddec53d84ef9bebd06847d3b0885946f5b
BinaryBun <chibishew2018@gmail.com>
2022-06-08 14:42:38 +0300

    grades methods

--- polykek.py
+++ polykek.py
@@ -68,3 +68,74 @@ class polykek(object):
 				data[element.text] = element.find("a")["href"]
 
 			return data
+
+	def get_ads_courses(self, MoodleSession=""):
+		'''
+			Get courses from main page
+		'''
+		if MoodleSession == "" and self.token == "":
+			return "Error: tokin is empty"
+		else:
+			data = {}
+			MoodleSession = max(MoodleSession, self.token, key=lambda i:len(i))
+			url = f"https://online.mospolytech.ru/"
+			cookies = {"MoodleSession": MoodleSession}  # valid moodlesession
+
+			content = requests.post(url, cookies=cookies)
+			sp = BeautifulSoup(content.text, "html.parser")
+			elements = sp.findAll("div", {"data-type": "1"})
+			for element in elements:
+				course_name = element.find("div", {"class": "clt_course_title_content"}).text.strip()
+				data[course_name] = element.find("a")["href"]
+
+			return data
+
+	def get_grades(self, MoodleSession=""):
+		'''
+			Get list of courses grades 
+
+			!!! "Итоговая оценка за курс" is missing (<span>)
+		'''
+		if MoodleSession == "" and self.token == "":
+			return "Error: tokin is empty"
+		else:
+			data = {}
+			MoodleSession = max(MoodleSession, self.token, key=lambda i:len(i))
+			url = f"https://online.mospolytech.ru/grade/report/overview"
+			cookies = {"MoodleSession": MoodleSession}  # valid moodlesession
+
+			content = requests.post(url, cookies=cookies)
+			sp = BeautifulSoup(content.text, "html.parser")
+			elements = sp.find("tbody").findAll("tr", {"class": ""})
+			for element in elements:
+				course_tag = element.find("a")
+				data[course_tag.text.strip()] = course_tag["href"]
+
+			return data
+
+	def get_grades_by_url(self, url, MoodleSession=""):
+		'''
+			Get list of grades bu course
+		'''
+		if MoodleSession == "" and self.token == "":
+			return "Error: tokin is empty"
+		else:
+			data = []
+			MoodleSession = max(MoodleSession, self.token, key=lambda i:len(i))
+			cookies = {"MoodleSession": MoodleSession}  # valid moodlesession
+
+			content = requests.post(url, cookies=cookies)
+			sp = BeautifulSoup(content.text, "html.parser")
+			elements = sp.find("tbody").findAll("tr")
+			for element in elements[1:]:  # elements[0] it`s name of course
+				grades_data = element.findAll("td")
+				try:
+					data.append([element.find("th").find("a").text, grades_data[0].text, grades_data[1].text,
+																	grades_data[2].text, grades_data[3].text,
+																	grades_data[4].text, grades_data[5].text])
+				except AttributeError:
+					#print([url, element])  # log
+					pass
+
+			return data
+
